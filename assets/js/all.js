ffa0fcd3be4c2dc159d059aaf24d97156a7a8040
yinmin <alice0050722@gmail.com>
2020-06-18 23:37:39 +0800

    Update 2020-06-18T15:37:36.876Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,6 +3,24 @@
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 4,
   spaceBetween: 30,
-  centeredSlides: true
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+$(window).resize(function () {
+  if ($(window).width() > 768) {
+    $('#multiCollapseExample1').collapse('show');
+  } else {
+    $('#multiCollapseExample1').collapse('hide');
+  }
+});
+$('.navbar-collapse .navbar-list li .back').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
+$('.navbar-collapse .navbar-list li .cart').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+  $('#cartModal').modal('show');
 });
 //# sourceMappingURL=all.js.map
